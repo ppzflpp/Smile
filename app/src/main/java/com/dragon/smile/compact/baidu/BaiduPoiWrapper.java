0353dc6ae0e19e1f9373c8d641ceea0d6cb19f52
zhangfeilong <499360256@qq.com>
2015-04-12 17:38:57 +0800

    增加功能

--- app/src/main/java/com/dragon/smile/compact/baidu/BaiduPoiWrapper.java
+++ app/src/main/java/com/dragon/smile/compact/baidu/BaiduPoiWrapper.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.location.Location;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.cloud.CloudListener;
+import com.baidu.mapapi.cloud.CloudManager;
+import com.baidu.mapapi.cloud.CloudSearchResult;
+import com.baidu.mapapi.cloud.DetailSearchResult;
+import com.baidu.mapapi.cloud.NearbySearchInfo;
 import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.search.core.PoiInfo;
 import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
@@ -27,9 +32,22 @@ import java.util.List;
 public class BaiduPoiWrapper implements PoiWrapper {
 
     private final static String TAG = "BaiDuPoiWrapper";
+    private CloudListener mCloudListener = new CloudListener() {
+        @Override
+        public void onGetSearchResult(CloudSearchResult cloudSearchResult, int i) {
+            LogUtils.d(TAG, "cloud onGetSearchResult");
+        }
+
+        @Override
+        public void onGetDetailSearchResult(DetailSearchResult detailSearchResult, int i) {
+            LogUtils.d(TAG, "cloud onGetDetailSearchResult");
+        }
+    };
+    private final static boolean ENABLE_CLOUD_POI_SEARCH = false;
     private LatLng mLocation = null;
     private String mLocationString = null;
     private PoiSearch mPoiSearch = null;
+    private CloudManager mCloudManager = null;
     private SuggestionSearch mSuggestionSearch = null;
     private List<PoiDataCallback> mPoiDataCallbackList = new ArrayList<PoiDataCallback>();
     private List<BusinessData> mBusinessDataList = new ArrayList<>();
@@ -37,15 +55,19 @@ public class BaiduPoiWrapper implements PoiWrapper {
         @Override
         public void onGetPoiResult(PoiResult poiResult) {
             List<PoiInfo> lists = poiResult.getAllPoi();
+            if (lists == null)
+                return;
+
             for (PoiInfo info : lists) {
                 BusinessData data = new BusinessData();
                 data.id = info.uid;
                 data.name = info.name;
                 data.address = info.address;
                 data.phone = info.phoneNum;
-                data.latitude = mLocation.latitude;
-                data.longitude = mLocation.longitude;
+                data.latitude = info.location.latitude;
+                data.longitude = info.location.longitude;
                 data.distance = getDistance(mLocation.latitude, mLocation.longitude, info.location.latitude, info.location.longitude);
+                LogUtils.d(TAG, "longitude = " + data.longitude + ",latitude = " + data.latitude);
                 mBusinessDataList.add(data);
             }
 
@@ -62,16 +84,32 @@ public class BaiduPoiWrapper implements PoiWrapper {
     public BaiduPoiWrapper(final Context context) {
         SDKInitializer.initialize(context);
 
-        mPoiSearch = PoiSearch.newInstance();
-        mPoiSearch.setOnGetPoiSearchResultListener(mOnGetPoiSearchResultListener);
+        if (ENABLE_CLOUD_POI_SEARCH) {
+            mCloudManager = CloudManager.getInstance();
+            mCloudManager.init(mCloudListener);
+            LogUtils.d(TAG, "--------------------------init------------------------");
+        } else {
+            mPoiSearch = PoiSearch.newInstance();
+            mPoiSearch.setOnGetPoiSearchResultListener(mOnGetPoiSearchResultListener);
+        }
 
     }
 
     @Override
     public void start() {
-        mPoiSearch.searchNearby(new PoiNearbySearchOption().keyword(SearchConstant.SEARCH_KEY_WORD)
-                .radius(SearchConstant.SEARCH_RADIUS).pageCapacity(SearchConstant.SEARCH_PAGE_CAPACITY)
-                .location(mLocation));
+        if (ENABLE_CLOUD_POI_SEARCH) {
+            NearbySearchInfo info = new NearbySearchInfo();
+            info.ak = "1ef16c3021f26091f465617ae4f790eb";
+            info.pageSize = SearchConstant.SEARCH_PAGE_CAPACITY;
+            info.radius = SearchConstant.SEARCH_RADIUS;
+            info.location = new StringBuilder().append(mLocation.longitude).append(",").append(mLocation.latitude).toString();
+            mCloudManager.nearbySearch(info);
+            LogUtils.d(TAG, "--------------------------nearbySearch------------------------" + info.location.toString());
+        } else {
+            mPoiSearch.searchNearby(new PoiNearbySearchOption().keyword(SearchConstant.SEARCH_KEY_WORD)
+                    .radius(SearchConstant.SEARCH_RADIUS).pageCapacity(SearchConstant.SEARCH_PAGE_CAPACITY)
+                    .location(mLocation));
+        }
     }
 
     @Override
